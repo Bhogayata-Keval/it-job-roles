68afaf2852417444a98a5e6d57e987494ddb17f7
Keval <bhogayatakb@gmail.com>
2022-01-07 14:49:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-function showRandomJobRole(message) {
-    if (message === undefined) throw new Error("No Message Found");
+function showRandomJobRole() {
     console.log("Developer:\nIs a person who writes code to build an application, using a suitable programming language !")
 };
 
